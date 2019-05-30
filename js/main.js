19594b4557f197b9f7ec894db8bde2a5bdd8b86f
RahulNathani49 <nathanirahul49@gmail.com>
2019-05-30 16:36:42 +0530

    window

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-var clk = 1;
-document.addEventListener('mousemove', function (e) {
-    var n = document.createElement("div");
-    if (clk == 1) {
-        n.style.backgroundColor = "red";
-    } else if (clk == 2) {
-        n.style.backgroundColor = "blue";
+$(document).ready(function () {
 
-    } else if (clk == 3) {
-        n.style.backgroundColor = "green";
-    } else if (clk == 4) {
-        n.style.backgroundColor = "yellow";
-    } else if (clk == 5) {
-        n.style.backgroundColor = "black";
-    } else if (clk == 6) {
-        clk = 1;
-    }
-    var x = e.clientX;
-    var y = e.clientY;
-    n.style.height = y + "px";
-    n.style.width = x + "px";
-    window.onclick = function () {
-        clk++;
+
+    $(document).click(function (t) {
+
+        var n = document.createElement("div");
+
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+        n.style.backgroundColor = getRandomColor();
         document.getElementById("bod").appendChild(n);
-    }
+        $(document).mousemove(function (e) {
+
+            $("div:last-child").css({
+                "height": e.clientY,
+                "width": e.clientX,
+            });
+
+
+
+        });
+
+    });
 });
